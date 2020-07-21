ed429887a3f38f66a9d782442e8a634b51b5e16b
jMushmyH <jMushmyH@email.com>
2020-07-21 22:28:04 +0300

    +demo

--- src/main/java/com/epam/rd/java/basic/practice4/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part3.java
@@ -1,11 +1,28 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Part3 {
+
 
     public static void main(String[] args) {
         // regex task
-
+        Map<String, Integer> intValues = new HashMap<>();
+        Map<String, Character> charValues = new HashMap<>();
+        Map<String, Double> doubleValues = new HashMap<>();
+        Map<String, String> stringValues = new HashMap<>();
 
     }
 
+    private static String part3Path = "part3.txt";
+
+    private static void sort2Maps() {
+        String input = Demo.readFile(part3Path);
+        String regexFindIntegers = "(?<=\\s)\\d+(?=\\s)";
+        String regexFindCharacters = "";
+        String regexFindDoubles = "";
+        String regexFindStrings = "\\b[A-zА-яЁё]{2,}?\\b";
+
+    }
 }
