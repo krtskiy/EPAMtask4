c52b87b4fd48f8a3e5205d30410a004d661d371b
jMushmyH <jMushmyH@email.com>
2020-07-21 19:20:14 +0300

    part2 test1

--- src/main/java/com/epam/rd/java/basic/practice4/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part2.java
@@ -1,10 +1,69 @@
 package com.epam.rd.java.basic.practice4;
 
-public class Part2 {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.security.SecureRandom;
+import java.util.*;
 
+public class Part2 {
     public static void main(String[] args) {
-        // Math.random 10 times to a part2, then read part2, then sort it and output to part2_sorted
+        writeTenRandomNumbersToFile();
+        demonstrateUnsorted();
+        sortRandomNumberFromFile();
+        demonstrateSorted();
+    }
+
+    private static String randomUnsorted = "part2.txt";
+    private static String randomSorted = "part2_sorted.txt";
+
+    private static void writeTenRandomNumbersToFile() {
+        StringBuilder numbers = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        try {
+            PrintWriter pw = new PrintWriter(randomUnsorted);
+            for (int i = 0; i < 10; i++) {
+                int rand = secureRandom.nextInt(50);
+                numbers.append(rand).append(" ");
+            }
+            pw.print(numbers);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); //NOSONAR
+        }
+    }
+
+    private static void sortRandomNumberFromFile() {
+        StringBuilder numbers = new StringBuilder();
+        List<Integer> inputSet = new ArrayList<>();
+        String input = Demo.readFile(randomUnsorted);
+        String[] inputArr = input.split(" ");
+        for (String s : inputArr) {
+            inputSet.add(Integer.parseInt(s));
+        }
+        try {
+            Collections.sort(inputSet);
+            for (Integer numbs : inputSet) {
+                numbers.append(numbs).append(" ");
+            }
+            PrintWriter pw = new PrintWriter(randomSorted);
+            pw.print(numbers);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); //NOSONAR
+        }
 
     }
+
+    private static void demonstrateUnsorted() {
+        String result = "input ==> ";
+        System.out.println(result + Demo.readFile(randomUnsorted)); //NOSONAR
+
+    }
+
+    private static void demonstrateSorted() {
+        String result = "output ==> ";
+        System.out.println(result + Demo.readFile(randomSorted)); //NOSONAR
+    }
+
 
 }
