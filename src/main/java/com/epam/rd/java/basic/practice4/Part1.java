4aca8b8f5c449e6280686e82b8020f827084def8
jMushmyH <jMushmyH@email.com>
2020-07-21 13:18:14 +0300

    txt files

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -3,6 +3,13 @@ package com.epam.rd.java.basic.practice4;
 public class Part1 {
 
     public static void main(String[] args) {
+        // split string into string[]
+        // if string[i].length > 4 ==> string[i] = string[i].substring(2, s2.length());
+        // StringBuilder result
+
+        String s1 = "word";
+        String s2 = "ridiculous";
+        System.out.println(s2.substring(2, s2.length()));
 
     }
 
