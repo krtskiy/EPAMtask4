c96feaa82dc30f0248e3c47a6eacc223be2e017e
jMushmyH <jMushmyH@email.com>
2020-07-22 23:29:10 +0300

    фыва

--- src/main/java/com/epam/rd/java/basic/practice4/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part5.java
@@ -1,9 +1,18 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Part5 {
 
     public static void main(String[] args) {
-        // just my empty main method
+        Locale localeRu = new Locale("ru");
+        ResourceBundle bundleRu = ResourceBundle.getBundle("resources", localeRu);
+        System.out.println(bundleRu.getString("table"));
+        Locale localeEn = new Locale("en");
+        ResourceBundle bundleEn = ResourceBundle.getBundle("resources", localeEn);
+        System.out.println(bundleEn.getString("table"));
+
 
     }
 
