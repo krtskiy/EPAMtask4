bd230f4daa5ab393c7e38fad7d6e766d6fa151db
jMushmyH <jMushmyH@email.com>
2020-07-22 17:14:21 +0300

    part4

--- src/main/java/com/epam/rd/java/basic/practice4/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part4.java
@@ -1,18 +1,22 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part4 {
 
     public static void main(String[] args) {
-        // just my empty main method
-        String lineSeparator = System.lineSeparator();
         String input = Demo.readFile("part4.txt");
         Pattern pattern = Pattern.compile("(?U)[A-ZА-ЯЁ].+?\\.");
         Matcher matcher = pattern.matcher(input);
+        List<String> resultList = new ArrayList<>();
         while (matcher.find()) {
-            System.out.print(matcher.group() + lineSeparator);
+            resultList.add(matcher.group());
+        }
+        for (int i = 0; i < resultList.size(); i++) {
+            System.out.println(resultList.get(i));
         }
     }
 
