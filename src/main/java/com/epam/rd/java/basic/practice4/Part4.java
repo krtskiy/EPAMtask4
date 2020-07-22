f7dd4c034436b4096fdf2d1d0c6fb1e66d65a409
jMushmyH <jMushmyH@email.com>
2020-07-22 23:08:01 +0300

    Update Part4.java

--- src/main/java/com/epam/rd/java/basic/practice4/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part4.java
@@ -7,15 +7,10 @@ public class Part4 {
 
     public static void main(String[] args) {
         String input = Demo.readFile("part4.txt");
-        Pattern pattern = Pattern.compile("(?U)[A-ZА-ЯЁ].+?\\.");
+        Pattern pattern = Pattern.compile("(?Us)[A-ZА-ЯЁ].+?\\.");
         Matcher matcher = pattern.matcher(input);
-        String[] result = new String[20];
-        int index = 0;
         while (matcher.find()) {
-            result[index++] = matcher.group();
-        }
-        for (int i = 0; i < index; i++) {
-            System.out.println(result[i]);
+            System.out.println(matcher.group());
         }
     }
 
