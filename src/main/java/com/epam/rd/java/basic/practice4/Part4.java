62459cc6a26d29d551402ec4aa16e2a57ba5196d
jMushmyH <jMushmyH@email.com>
2020-07-22 17:07:48 +0300

    part4

--- src/main/java/com/epam/rd/java/basic/practice4/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part4.java
@@ -7,11 +7,12 @@ public class Part4 {
 
     public static void main(String[] args) {
         // just my empty main method
+        String lineSeparator = System.lineSeparator();
         String input = Demo.readFile("part4.txt");
         Pattern pattern = Pattern.compile("(?U)[A-ZА-ЯЁ].+?\\.");
         Matcher matcher = pattern.matcher(input);
         while (matcher.find()) {
-            System.out.println(matcher.group());
+            System.out.print(matcher.group() + lineSeparator);
         }
     }
 
